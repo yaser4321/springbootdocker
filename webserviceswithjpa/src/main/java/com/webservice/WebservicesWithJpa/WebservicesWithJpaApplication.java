951c685d29443cccb74626fe013b404cda66f8fd
yaser4321 <yaser4321@gmail.com>
2020-08-01 17:23:34 +0530

    sys added

--- webserviceswithjpa/src/main/java/com/webservice/WebservicesWithJpa/WebservicesWithJpaApplication.java
+++ webserviceswithjpa/src/main/java/com/webservice/WebservicesWithJpa/WebservicesWithJpaApplication.java
@@ -10,4 +10,6 @@ public class WebservicesWithJpaApplication {
 		SpringApplication.run(WebservicesWithJpaApplication.class, args);
 	}
 // comments updated
+	// intellij branch
+	System.out.println("Hello");
 }
